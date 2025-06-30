65d1f49b99b8b9e792644b1f4307ab1a31f3a0d3
FaridEspinoza <farid23jos@outlook.com>
2025-06-30 16:49:28 -0600

    Lista Casos
    Se creo la lista  casos con lo necesario

---
+++ src/Casos/ListCasos.java
@@ -0,0 +1,36 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Casos;
+import Listas.Lista;
+
+/**
+ *
+ * @author Farid
+ */
+public class ListCasos {
+    
+    private Caso[] casos = new Caso[100];
+    private int cantidad;
+    private int max;
+    
+    public void add(Caso caso) {
+        max = casos.length;
+        cantidad = 0;
+        if (cantidad < max) {
+            casos[cantidad++] = caso;
+        }
+    }
+
+    public int contarCasosPorPrioridad(Prioridad prioridad) {
+        int contador = 0;
+        for (int i = 0; i < max; i++) {
+            if (casos[i].getPrioridad() == prioridad) {
+                contador++;
+            }
+        }
+        return contador;
+    }
+    
+}
